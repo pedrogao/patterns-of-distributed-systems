59fb32601e38a21040d428ca4b9bfd76811a3700
pedrogao <1312342604@qq.com>
2023-10-20 18:36:23 +0800

    fix: add base config for site

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -5,6 +5,8 @@ export default defineUserConfig({
   title: "Patterns of Distributed Systems",
   description:
     "《分布式系统模式》（Patterns of Distributed Systems）是 Unmesh Joshi 编写的一系列关于分布式系统实现的文章。这个系列的文章采用模式的格式，介绍了像 Kafka、Zookeeper 这种分布式系统在实现过程采用的通用模式，是学习分布式系统实现的基础。",
+  base: "/patterns-of-distributed-systems/",
+
   theme: defaultTheme({
     logo: "https://martinfowler.com/articles/patterns-of-distributed-systems/card.png",
     repo: "https://github.com/pedrogao/patterns-of-distributed-systems",
